855cafdf1ab3e5fda9dcc6fece428d972dacba71
Artem Prosvetov <arteom.prosvetov@mail.ru>
2017-04-10 17:45:14 +0000

    Apply fixes from StyleCI

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,8 @@
 namespace App\Exceptions;
 
 use Exception;
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\QueryException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -54,7 +54,7 @@ class Handler extends ExceptionHandler
         if ($exception instanceof QueryException) {
             return redirect()->back()
                 ->with([
-                'status' => 'Данная почтв уже занята!'
+                'status' => 'Данная почтв уже занята!',
             ], 500);
         }
 
